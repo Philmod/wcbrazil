5aa6988fe25e310f9a9df20bec59fadcd23225f9
Philmod <philmod@storify.com>
2014-05-11 22:10:37 -0700

    package.json and remove node_modules

---
+++ _index.js
@@ -0,0 +1,17 @@
+// http://www.livefyre.com/install/
+
+/**
+ * Dependencies.
+ */
+var async = require('async');
+var scoreScraper = require('./lib/scraper-group.js')();
+
+/**
+ * Variables.
+ */
+var groups = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+async.concatSeries(groups, scoreScraper.scrap, function(err, results) {
+  if (err) console.log('error : ', err);
+  console.log('results scraping : ', results, results.length);
+});
