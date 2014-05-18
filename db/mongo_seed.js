80c8d8625182888822111d84171432c560a2e6e6
Philmod <philmod@storify.com>
2014-05-17 20:04:23 -0700

    seed db

---
+++ db/mongo_seed.js
@@ -0,0 +1,584 @@
+var games = [
+  {
+    "group": "A",
+    "teams": [
+      "Brazil",
+      "Croatia"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-12T20:00:00.000Z")
+  },
+  {
+    "group": "A",
+    "teams": [
+      "Mexico",
+      "Cameroon"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-13T16:00:00.000Z")
+  },
+  {
+    "group": "A",
+    "teams": [
+      "Brazil",
+      "Mexico"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-17T19:00:00.000Z")
+  },
+  {
+    "group": "A",
+    "teams": [
+      "Cameroon",
+      "Croatia"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-18T22:00:00.000Z")
+  },
+  {
+    "group": "A",
+    "teams": [
+      "Cameroon",
+      "Brazil"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-23T20:00:00.000Z")
+  },
+  {
+    "group": "A",
+    "teams": [
+      "Croatia",
+      "Mexico"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-23T20:00:00.000Z")
+  },
+  {
+    "group": "B",
+    "teams": [
+      "Spain",
+      "Netherlands"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-13T19:00:00.000Z")
+  },
+  {
+    "group": "B",
+    "teams": [
+      "Chile",
+      "Australia"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-13T22:00:00.000Z")
+  },
+  {
+    "group": "B",
+    "teams": [
+      "Australia",
+      "Netherlands"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-18T16:00:00.000Z")
+  },
+  {
+    "group": "B",
+    "teams": [
+      "Spain",
+      "Chile"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-18T19:00:00.000Z")
+  },
+  {
+    "group": "B",
+    "teams": [
+      "Australia",
+      "Spain"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-23T16:00:00.000Z")
+  },
+  {
+    "group": "B",
+    "teams": [
+      "Netherlands",
+      "Chile"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-23T16:00:00.000Z")
+  },
+  {
+    "group": "C",
+    "teams": [
+      "Colombia",
+      "Greece"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-14T16:00:00.000Z")
+  },
+  {
+    "group": "C",
+    "teams": [
+      "Ivory Coast",
+      "Japan"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-15T01:00:00.000Z")
+  },
+  {
+    "group": "C",
+    "teams": [
+      "Colombia",
+      "Ivory Coast"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-19T16:00:00.000Z")
+  },
+  {
+    "group": "C",
+    "teams": [
+      "Japan",
+      "Greece"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-19T22:00:00.000Z")
+  },
+  {
+    "group": "C",
+    "teams": [
+      "Greece",
+      "Ivory Coast"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-24T20:00:00.000Z")
+  },
+  {
+    "group": "C",
+    "teams": [
+      "Japan",
+      "Colombia"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-24T20:00:00.000Z")
+  },
+  {
+    "group": "D",
+    "teams": [
+      "Uruguay",
+      "Costa Rica"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-14T19:00:00.000Z")
+  },
+  {
+    "group": "D",
+    "teams": [
+      "England",
+      "Italy"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-14T22:00:00.000Z")
+  },
+  {
+    "group": "D",
+    "teams": [
+      "Uruguay",
+      "England"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-19T19:00:00.000Z")
+  },
+  {
+    "group": "D",
+    "teams": [
+      "Italy",
+      "Costa Rica"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-20T16:00:00.000Z")
+  },
+  {
+    "group": "D",
+    "teams": [
+      "Costa Rica",
+      "England"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-24T16:00:00.000Z")
+  },
+  {
+    "group": "D",
+    "teams": [
+      "Italy",
+      "Uruguay"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-24T16:00:00.000Z")
+  },
+  {
+    "group": "E",
+    "teams": [
+      "Switzerland",
+      "Ecuador"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-15T16:00:00.000Z")
+  },
+  {
+    "group": "E",
+    "teams": [
+      "France",
+      "Honduras"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-15T19:00:00.000Z")
+  },
+  {
+    "group": "E",
+    "teams": [
+      "Switzerland",
+      "France"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-20T19:00:00.000Z")
+  },
+  {
+    "group": "E",
+    "teams": [
+      "Honduras",
+      "Ecuador"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-20T22:00:00.000Z")
+  },
+  {
+    "group": "E",
+    "teams": [
+      "Ecuador",
+      "France"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-25T20:00:00.000Z")
+  },
+  {
+    "group": "E",
+    "teams": [
+      "Honduras",
+      "Switzerland"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-25T20:00:00.000Z")
+  },
+  {
+    "group": "F",
+    "teams": [
+      "Argentina",
+      "Bosnia-Herzegovina"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-15T22:00:00.000Z")
+  },
+  {
+    "group": "F",
+    "teams": [
+      "Iran",
+      "Nigeria"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-16T19:00:00.000Z")
+  },
+  {
+    "group": "F",
+    "teams": [
+      "Argentina",
+      "Iran"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-21T16:00:00.000Z")
+  },
+  {
+    "group": "F",
+    "teams": [
+      "Nigeria",
+      "Bosnia-Herzegovina"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-21T22:00:00.000Z")
+  },
+  {
+    "group": "F",
+    "teams": [
+      "Bosnia-Herzegovina",
+      "Iran"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-25T16:00:00.000Z")
+  },
+  {
+    "group": "F",
+    "teams": [
+      "Nigeria",
+      "Argentina"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-25T16:00:00.000Z")
+  },
+  {
+    "group": "G",
+    "teams": [
+      "Germany",
+      "Portugal"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-16T16:00:00.000Z")
+  },
+  {
+    "group": "G",
+    "teams": [
+      "Ghana",
+      "USA"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-16T22:00:00.000Z")
+  },
+  {
+    "group": "G",
+    "teams": [
+      "Germany",
+      "Ghana"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-21T19:00:00.000Z")
+  },
+  {
+    "group": "G",
+    "teams": [
+      "USA",
+      "Portugal"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-22T22:00:00.000Z")
+  },
+  {
+    "group": "G",
+    "teams": [
+      "Portugal",
+      "Ghana"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-26T16:00:00.000Z")
+  },
+  {
+    "group": "G",
+    "teams": [
+      "USA",
+      "Germany"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-26T16:00:00.000Z")
+  },
+  {
+    "group": "H",
+    "teams": [
+      "Belgium",
+      "Algeria"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-17T16:00:00.000Z")
+  },
+  {
+    "group": "H",
+    "teams": [
+      "Russia",
+      "South Korea"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-17T22:00:00.000Z")
+  },
+  {
+    "group": "H",
+    "teams": [
+      "Belgium",
+      "Russia"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-22T16:00:00.000Z")
+  },
+  {
+    "group": "H",
+    "teams": [
+      "South Korea",
+      "Algeria"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-22T19:00:00.000Z")
+  },
+  {
+    "group": "H",
+    "teams": [
+      "Algeria",
+      "Russia"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-26T20:00:00.000Z")
+  },
+  {
+    "group": "H",
+    "teams": [
+      "South Korea",
+      "Belgium"
+    ],
+    "score": [
+      0,
+      0
+    ],
+    "time": new Date("2014-06-26T20:00:00.000Z")
+  }
+];
+
+db.games.remove();
+games.forEach(function(g) {
+  print (g)
+  db.games.insert(g);
+});
