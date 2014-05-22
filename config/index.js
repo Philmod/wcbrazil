260edd682d268e85bfd24096558daaad9477bc66
Philmod <philmod@storify.com>
2014-05-22 00:03:30 -0700

    basic style

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = function(server, next) {
    */
   server.config.scraping = {
       groups: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    , dt: 5*1000
+    , dt: 5*1000*1000
   };
 
   /**
