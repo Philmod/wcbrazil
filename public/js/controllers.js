ee55390c9331b49cf2104f9d573b0315c356e5f2
Philmod <philmod@storify.com>
2014-05-27 23:14:31 -0700

    browser specific css [ie8]

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,7 +4,12 @@
 
 angular.module('wcbrazilApp.controllers', [])
   .controller('AppCtrl', function ($scope, socket) {
-    
+    var browser = get_browser();
+    var browserV = get_browser_version();
+    if (browser === 'MSIE' && browserV === '8') {
+      var root = document.documentElement;
+      root.className += " ie8";
+    }
   })
   .controller('GamesCtrl', function ($scope, socket) {
     // Store games.
@@ -77,5 +82,35 @@ var flip = function(element) {
             .addClass("play");
     }
 
+}
+
+function get_browser(){
+  var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+  if(/trident/i.test(M[1])){
+      tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+      return 'IE '+(tem[1]||'');
+      }   
+  if(M[1]==='Chrome'){
+      tem=ua.match(/\bOPR\/(\d+)/)
+      if(tem!=null)   {return 'Opera '+tem[1];}
+      }   
+  M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+  return M[0];
+}
+
+function get_browser_version(){
+  var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];                                                                                                                         
+  if(/trident/i.test(M[1])){
+      tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
+      return 'IE '+(tem[1]||'');
+      }
+  if(M[1]==='Chrome'){
+      tem=ua.match(/\bOPR\/(\d+)/)
+      if(tem!=null)   {return 'Opera '+tem[1];}
+      }   
+  M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+  return M[1];
 }
 
