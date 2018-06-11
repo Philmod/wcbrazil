5f3e159e21eafb4137a3a17bea67e2b068b52c61
Philmod <philippe.modard@gmail.com>
2018-06-11 15:22:57 -0400

    set websocket path

--- public/js/services.js
+++ public/js/services.js
@@ -4,6 +4,9 @@
 
 angular.module('russia2018App.services', []).
   factory('socket', function (socketFactory) {
-    return socketFactory();
+    return socketFactory({
+      // TODO(philmod): set this link in the config.
+      ioSocket: io.connect('/russia2018/socket.io');
+    });
   }).
   value('version', '0.1');
